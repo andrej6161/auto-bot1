af78a595f358fc9402f020fc155c65fa6d1a1c1b
andrej6161 <andrej.dragovic007@gmail.com>
2025-12-11 23:43:28 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 import requests
-import time
 
 TOKEN = "AAGbSf4tAl3EabHVc95olm__XIIhbXyTSZU"
 CHAT_ID = "8457305838"
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text}
-    requests.post(url, json=payload)
-
-while True:
-    send_message("Bot radi 24/7 ✅")
-    time.sleep(3600)  # šalje poruku na svakih sat vremena
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+payload = {"chat_id": CHAT_ID, "text": "Test poruka – ako ovo vidiš, bot radi ✅"}
+requests.post(url, json=payload)
